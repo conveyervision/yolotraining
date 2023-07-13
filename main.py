fcd90d5d1a79b1668b5a680f63849dcf016656bd
saadimalik211 <saadmali@gensosekai.com>
2023-07-12 19:21:31 -0700

    some qol tweaks

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-import torch
+# menu.py
 from trainModel import train_yolo
+from runModel import run_yolo
 
-def print_hi(name):
-    print(f'Hi, {name}')
+def main_menu():
+    print("Welcome to YoloTraining!")
+    print("Please choose an option from the menu below:")
+    print("1. Train model")
+    print("2. Run model")
+
+    choice = input("Enter your choice (1-2): ")
+
+    if choice == "1":
+        model_size = input("Enter model size (X/L/M/S/N or A for All, default is M): ")
+        data = input("Enter data file name (default is 'custom.yaml'): ")
+        epochs = int(input("Enter number of epochs (default is 10): "))
+        imgsz = int(input("Enter image size (default is 960): "))
+        batch = int(input("Enter batch size (default is 10): "))
+        train_yolo(model_size, data, epochs, imgsz, device=0, batch=batch)
+    elif choice == "2":
+        run_yolo()
+    else:
+        print("Invalid choice, please enter a valid option.")
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("Cuda available: " + str(torch.cuda.is_available()))
-    print("Num Devices: " + str(torch.cuda.device_count()))
-    print("Device: " + str(torch.cuda.get_device_name()))
-    train_yolo()
+    main_menu()
--- trainModel.py
+++ trainModel.py
@@ -1,6 +1,12 @@
+# trainModel.py
 from ultralytics import YOLO
 
-def train_yolo():
-    model = YOLO('yolov8x.pt')
-    model.train(data='custom.yaml', epochs=100, imgsz=960, device=0, batch=2)
+def train_yolo(model_size='m', data='custom.yaml', epochs=10, imgsz=960, device=0, batch=10):
+    if model_size.lower() == 'a':  # for all models
+        model_sizes = ['x', 'l', 'm', 's', 'n']
+    else:
+        model_sizes = [model_size.lower()]
 
+    for size in model_sizes:
+        model = YOLO(f'yolov8{size}.pt')
+        model.train(data=data, epochs=epochs, imgsz=imgsz, device=device, batch=batch)
